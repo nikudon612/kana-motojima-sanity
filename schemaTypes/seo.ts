edebc8105d9ac8869bf850f06c5e9cfe29e9bc98
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-02-12 00:36:16 -0500

    resolved error with doc type

--- schemaTypes/seo.ts
+++ schemaTypes/seo.ts
@@ -1,32 +1,32 @@
-import { defineField, defineType } from "sanity";
+import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: "seo",
-  title: "SEO",
-  type: "object",
+  name: 'seo',
+  title: 'SEO',
+  type: 'document',
   fields: [
     defineField({
-      name: "title",
-      title: "Meta Title",
-      type: "string",
-      validation: (Rule) => Rule.max(60).warning("Titles should be under 60 characters."),
+      name: 'title',
+      title: 'Meta Title',
+      type: 'string',
+      validation: (Rule) => Rule.max(60).warning('Titles should be under 60 characters.'),
     }),
     defineField({
-      name: "description",
-      title: "Meta Description",
-      type: "text",
-      validation: (Rule) => Rule.max(160).warning("Descriptions should be under 160 characters."),
+      name: 'description',
+      title: 'Meta Description',
+      type: 'text',
+      validation: (Rule) => Rule.max(160).warning('Descriptions should be under 160 characters.'),
     }),
     defineField({
-      name: "ogImage",
-      title: "Open Graph Image",
-      type: "image",
-      options: { hotspot: true },
+      name: 'ogImage',
+      title: 'Open Graph Image',
+      type: 'image',
+      options: {hotspot: true},
     }),
     defineField({
-      name: "canonicalUrl",
-      title: "Canonical URL",
-      type: "url",
+      name: 'canonicalUrl',
+      title: 'Canonical URL',
+      type: 'url',
     }),
   ],
-});
+})
