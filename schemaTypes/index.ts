9ffe1e8e721b27c2b37bd7fe96bd95cc143e72b0
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-02-12 00:30:58 -0500

    updated index for seo

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,6 @@ import photo from './photo';
 import about from './about';
 import homepageGallery from './homepageGallery';
 import project from './project';
+import seo from './seo';
 
-export const schemaTypes = [blockContent, about, photo, homepageGallery, project];
+export const schemaTypes = [blockContent, about, photo, homepageGallery, project, seo];
